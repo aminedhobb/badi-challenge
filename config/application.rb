c731e0f7282922b530ba4e09e338e29564dbead1
aminedhobb <amine.dhobb@gmail.com>
2018-11-25 17:51:22 +0100

    precompiling assets

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module BadiCodingChallengeBackend
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.assets.initialize_on_precompile = false
 
     # needed for devise
     # config.middleware.use ActionDispatch::Flash
@@ -28,7 +29,6 @@ module BadiCodingChallengeBackend
     # config.middleware.use ActionDispatch::Cookies
 
     # Rails.application.config.session_store :cache_store, key: '_app'
-    
 
     # config.middleware.use ActionDispatch::Session::RedisSessionStore
     # Settings in config/environments/* take precedence over those specified here.
